8e9e65c15cf768c94c237c0d40f3f1762f68278c
zhouxiaotian <1282347298@qq.com>
2016-09-07 18:35:46 +0800

    AJAX和NODE

--- 第八期正式课课件/第七周/第二天/backups/server1521.js
+++ 第八期正式课课件/第七周/第二天/backups/server1521.js
@@ -7,8 +7,11 @@ var server1 = http.createServer(function (request, response) {
         query = urlObj['query'];
 
     //->服务器端处理客户端静态资源文件的请求
+    //->静态资源文件:HTML/CSS/JS/PNG/GIF...
+    //->前提:SERVER.JS在当前项目的根目录下
     var reg = /\.(HTML|CSS|JS|ICO)/i;
     if (reg.test(pathname)) {
+        //->通过请求资源文件的后缀名获取对应的MIME类型
         var suffix = reg.exec(pathname)[1].toUpperCase();
         var suffixMIME = 'text/plain';
         switch (suffix) {
@@ -22,14 +25,14 @@ var server1 = http.createServer(function (request, response) {
                 suffixMIME = 'text/javascript';
                 break;
         }
+
         try {
             var conFile = fs.readFileSync('.' + pathname, 'utf-8');
-            response.writeHead(200, {
-                'content-type': suffixMIME + ';charset=utf-8;',
-                'zhufeng': 'zhufengpeixun'
-            });
+            //->在返回给客户端内容的时候,还需要告诉客户端文件的MIME类型
+            response.writeHead(200, {'content-type': suffixMIME + ';charset=utf-8;'});
             response.end(conFile);
         } catch (e) {
+            //->当前客户端请求的资源文件不存在
             response.writeHead(404, {'content-type': 'text/plain;charset=utf-8;'});
             response.end('request file is not found~');
         }
