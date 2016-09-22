c5c02d3b2298ba3657e1dff884592b6d3eb97ee9
zhouxiaotian <1282347298@qq.com>
2016-09-22 13:01:53 +0800

    补课-弹性势能动画

--- 第八期正式课课件/第九周/项目实战-腾讯看比赛/js/backups/index1246.js
+++ 第八期正式课课件/第九周/项目实战-腾讯看比赛/js/backups/index1246.js
@@ -35,42 +35,29 @@ var menuRender = function () {
         $menuUL = $menu.children('ul'),
         menuScroll = null;
 
-    //->发布订阅模式:创建一个计划$menuPlain.add()/$menuPlain.remove()
-    var $menuPlain = $.Callbacks();
-
-    //->BIND HTML
-    $menuPlain.add(function (result) {
-        $menuUL.html(ejs.render($('#menuTemplate').html(), {menuData: result}));
-    });
-
-    //->ISCROLL
-    $menuPlain.add(function (result) {
-        menuScroll = new IScroll('.menu', {
-            scrollbars: true,
-            fadeScrollbars: true,
-            mouseWheel: true,
-            bounce: false
-        });
-    });
-
-    //->POSITION
-    $menuPlain.add(function (result) {
+    //->通过HASH值定位到具体的某一个MENU
+    function positionMenu() {
+        //->获取地址栏中的HASH值
         var obj = window.location.href.queryURLParameter(),
             hash = obj['HASH'] || 'nba';
+
+        //->获取UL中的所有的A标签,在所有的A中查找到HASH和我们获取结果一致的A:如果获取到了让当前的选中,没有获取到我们选中第一项
         var $link = $menuUL.find('a'),
             $curLink = $link.filter("[href='#" + hash + "']");
         $curLink = $curLink.length === 0 ? $link.eq(0) : $curLink;
         $curLink.addClass('bg');
-        menuScroll.scrollToElement($curLink[0], 300);
-    });
 
-    //->BIND EVENT
-    $menuPlain.add(function (result) {
+        //->让MENU区域滚动到选中的位置
+        menuScroll.scrollToElement($curLink[0], 300);
+    }
+
+    //->给所有的A绑定点击事件
+    function bindEvent() {
         var $link = $menuUL.find('a');
         $link.on('click', function () {
             $(this).addClass('bg').parent().siblings().children('a').removeClass('bg');
         });
-    });
+    }
 
     return {
         init: function () {
@@ -80,7 +67,22 @@ var menuRender = function () {
                 dataType: 'JSON',
                 success: function (result) {
                     if (result) {
-                        $menuPlain.fire(result);//->当数据获取成功后通知相关的方法依次执行,并且把获取的结果分别传递给每一个方法
+                        //->BIND HTML
+                        $menuUL.html(ejs.render($('#menuTemplate').html(), {menuData: result}));
+
+                        //->ISCROLL
+                        menuScroll = new IScroll('.menu', {
+                            scrollbars: true,
+                            fadeScrollbars: true,
+                            mouseWheel: true,
+                            bounce: false
+                        });
+
+                        //->通过HASH值定位到具体的某一个MENU
+                        positionMenu();
+
+                        //->绑定点击事件
+                        bindEvent();
                     }
                 }
             });
